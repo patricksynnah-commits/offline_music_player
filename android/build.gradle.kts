321ae497401e16c3bc9e6891229862f77a5660c3
Patrick <patricksynnah@gmail.com>
2026-05-11 08:44:54 +0000

    initial clean flutter music app

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,28 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-subprojects {
-    afterEvaluate {
-        if (this is com.android.build.gradle.LibraryExtension) {
-            namespace = "com.example.temp"
-        }
-    }
 }
